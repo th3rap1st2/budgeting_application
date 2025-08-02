ac2adcae148325dbc17fa6ddfafcaa167f12edbd
th3rap1st2 <wajehaj222@gmail.com>
2025-08-01 23:00:00 -0400

    fixing flask routes

--- Budgeting_App/backend/app.py
+++ Budgeting_App/backend/app.py
@@ -1,22 +1,22 @@
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 
-# Flask constructor takes the name of 
-# current module (__name__) as argument.
-app = Flask(__name__)
+app = Flask(__name__,
+             template_folder='../frontend/templates', 
+             static_folder='../frontend/static')
 
-# The route() function of the Flask class is a decorator, 
-# which tells the application which URL should call 
-# the associated function.
+
 @app.route('/')
-# ‘/’ URL is bound with hello_world() function.
-def hello_world():
-    return 'Hello World'
+def index():
+    return render_template('index.html')
 
-# main driver function
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/existing')
+def existing():
+    return render_template('existing.html')
+
+
 if __name__ == '__main__':
-
-    # run() method of Flask class runs the application 
-    # on the local development server.
-    app.run()
+    app.run(debug=True)
